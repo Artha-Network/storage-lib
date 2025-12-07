7911ba8d4a02c7a5d96affaa35ee35d63929bc24
Sampada1400 <Sampadadhungana@my.unt.edu>
2025-12-06 22:39:22 -0600

    Create index.ts to export modules and types

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export * from "./types";
+export * from "./hash";
+
+export {
+  ArweaveStore,
+  createArweaveStore,
+  arweaveStore,
+  type ArweaveConfig,
+} from "./arweaveStore";
+
+export {
+  IpfsStore,
+  createIpfsStore,
+  ipfsStore,
+  type IpfsConfig,
+} from "./ipfsStore";
